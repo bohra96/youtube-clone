df1b33c72d411e8c59473c4ca84bbb6063cde0fa
bohra96 <rahulrbk@outlook.com>
2025-03-02 20:12:51 +0530

    final version

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-const API_KEY = "";
+const API_KEY = "AIzaSyAKkoMKIdiQLIlSYQlYEVI1v5PylXohgEU";
 
 const videosList = document.getElementById("videos-list");
 
 const searchVideos = async () => {
   try {
-    const query = document.getElementById("yt-search").value;
+    const query = document.getElementById("yt-search").value.trim();
+    if (!query) return; // Don't search if query is empty
     console.log(query);
+    videosList.innerHTML =
+      '<div class="col-12 text-center"><p>Loading videos...</p></div>';
+    // Rest of your code
     const API_URL = `https://youtube.googleapis.com/youtube/v3/search?maxResults=12&type=video&videoDuration=long&q=${query}&key=${API_KEY}`;
 
     const res = await fetch(API_URL);
@@ -24,6 +28,7 @@ const displayVideos = (videos) => {
     col.classList.add("col-xl-6", "col-lg-6", "col-sm-12");
 
     const iframe = document.createElement("iframe");
+    iframe.width = "100%";
     iframe.src = `https://www.youtube.com/embed/${video.id.videoId}`;
     iframe.title = "YouTube video player";
     iframe.frameborder = "0";
